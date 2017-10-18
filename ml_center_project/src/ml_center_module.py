48ea3564c416430c2905aea0e68509db2b74b9b3
ammgithub <malyscheff@gmail.com>
2017-10-18 12:01:09 -0500

    First commit.

---
+++ ml_center_project/src/ml_center_module.py
@@ -0,0 +1,26 @@
+"""
+Created on October 18, 2017
+
+Machine learning and another center (other than the analytic center)
+
+"""
+__author__ = 'amm'
+__date__ = "Oct 05, 2017"
+__version__ = 0.0
+
+import numpy as np
+import pylab as plt
+
+np.set_printoptions(linewidth=100, edgeitems='all', suppress=True,
+                    precision=2)
+
+
+
+if __name__ == '__main__':
+    """
+    execfile('ml_center_module.py')
+    """
+    import os
+
+    os.chdir('C:\\Users\\amalysch\\PycharmProjects\\ml_center_repository\\ml_center_project\\src')
+    print "Alex"
